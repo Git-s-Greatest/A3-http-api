f69becc83d6feffba05b0a2f4401137998139c8f
Jose-Roman <josejroman@cpp.edu>
2021-09-23 14:18:58 -0700

    using the date fns npm package to print current date

--- JoseRoman-A3/src/components/create-todo.component.js
+++ JoseRoman-A3/src/components/create-todo.component.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { add, addDays,format,formatDuration, intervalToDuration } from 'date-fns';
 export default class CreateTodo extends Component {
     render() {
+        const date = new Date();
+        
+        const dateFormatted = format(date, "MM/dd/yyyy"); //prints current date
+        
         return (
             <div>
+                <p>{dateFormatted}</p>
                <a href="http://cs480-projects.github.io/teams-fall2021/Gits-Greatest/index.html">Class Coment Website</a>
+                
             </div>
         )
     }
