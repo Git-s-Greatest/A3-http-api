e0eb44d722f49376f9f712b34956392b3c3c60dc
Jose-Roman <77639863+Jose-Roman@users.noreply.github.com>
2021-10-10 20:52:13 -0700

    Add files via upload

--- JoseRoman-A3/src/setupTests.js
+++ JoseRoman-A3/src/setupTests.js
@@ -2,4 +2,14 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+//import '@testing-library/jest-dom';
+
+describe("Get GOAT NBA Player", function() {
+    var a;
+  
+    it("and so is a spec", function() {
+      a = "Kobe";
+  
+      expect(a).toBe("Kobe");
+    });
+  });
